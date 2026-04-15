830c6d6d1a0d9b009ed59f410818ce59a7922ee5
Adebayo Azeez <azeez.adedayo@yahoo.com>
2026-04-15 21:59:19 +0300

    Initial project setup

---
+++ src/AzureDevOpsServiceHookSoln.API/Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureDevOpsServiceHookSoln.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        private readonly ILogger<WebhookController> _logger;
+        public WebhookController(ILogger<WebhookController> logger)
+        {
+            _logger = logger;
+            
+        }
+
+        [HttpPost]
+        public IActionResult ReceiveWebhook([FromBody] object payload)
+        {
+            _logger.LogInformation($"Request {System.Text.Json.JsonSerializer.Serialize(payload)}");
+            return Ok(new
+            {
+                message = "Okay"
+            });
+        }
+    }
+}
